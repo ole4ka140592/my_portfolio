38503025b6844e55a685464f7cebbb7861ec8b73
ole4ka140592 <ole4ka140592@gmail.com>
2022-01-12 21:54:16 +0300

    changes were made to Main.module.css: .container

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,16 +2,22 @@ import React from 'react';
 import style from './Main.module.css';
 
 
-
 export const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={style.text}>
-                <span>Hi There</span>
-                <h1>I am Volha Mitsiurova</h1>
-                <p>Front-end developer</p>
+
+            <div className={style.container}>
+
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>I am Volha Mitsiurova</h1>
+                    <p>Front-end developer</p>
+                </div>
+                <div className={style.photo}></div>
+
             </div>
-            <div className={style.photo}></div>
+
+
         </div>
     )
 }
