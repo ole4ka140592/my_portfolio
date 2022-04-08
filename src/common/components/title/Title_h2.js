fe0a98cb7a6940ddd29163e2f7b0af325a95bd2f
ole4ka140592 <ole4ka140592@gmail.com>
2022-04-08 12:51:41 +0300

    add style for title-h2

---
+++ src/common/components/title/Title_h2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Title_h2.css';
+
+
+export const Title_h2 = () => {
+    return (
+        <div className={style.title}>
+            <h2>Projects</h2>
+        </div>
+    )
+}
+
