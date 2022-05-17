0fa40913cce9607db5a262b610ff2be747399e96
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-18 02:44:29 +0300

    add changes

--- src/main/Main.js
+++ src/main/Main.js
@@ -7,19 +7,14 @@ export const Main = () => {
 
     return (
         <div className={style.mainBlock}>
-
             <div className={styleContainer.container}>
-
                 <div className={style.text}>
                     {/*<span className={style.hi}>Hi There</span>*/}
                     <h2 className={style.name}>I am Volha Mitsiurova</h2>
                     <p className={style.frontendDeveloper}>Front-end Developer</p>
                 </div>
                 <div className={style.photo}></div>
-
             </div>
-
-
         </div>
     )
 }
