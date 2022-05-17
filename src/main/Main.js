12af92e0f7f3ee0dd0ee7b5df2a9cdc706d319dc
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-17 23:54:11 +0300

    add changes

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,6 +3,8 @@ import style from './Main.module.css';
 import styleContainer from '../common/styles/Container.module.css';
 
 export const Main = () => {
+
+
     return (
         <div className={style.mainBlock}>
 
