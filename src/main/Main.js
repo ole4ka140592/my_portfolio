0567f95d5608d50f51f9a854fb4cd424406329b7
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-20 23:19:16 +0300

    add photo in Main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
+import iImage from "../assets/image/I.jpg"
 
 export const Main = () => {
+
+    const i = {
+        backgroundImage: `url(${iImage})`
+    }
 
 
     return (
@@ -12,7 +17,7 @@ export const Main = () => {
                     <h2 className={style.name}>I am Volha Mitsiurova</h2>
                     <p className={style.frontendDeveloper}>Front-end Developer</p>
                 </div>
-                <div className={style.photo}></div>
+                <div className={style.photo} style={i}></div>
             </div>
         </div>
     )
