b1b26de116fd738df3aa669739ff01d5306ee363
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-22 01:17:50 +0300

    add icon in Skills

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,7 +3,21 @@ import style from './Skills.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import {Skill} from "./skill/Skill";
 import {Title_h2} from "../common/components/title/Title_h2";
-
+import reactImage from "../assets/image/react.png";
+import jsImage from "../assets/image/js.png";
+import tsImage from "../assets/image/typescript.png";
+import htmlImage from "../assets/image/html.png";
+import cssImage from "../assets/image/css.png";
+import sassImage from "../assets/image/sass.png";
+import restapiImage from "../assets/image/restapi.png";
+import reduxImage from "../assets/image/redux.png";
+import httpImage from "../assets/image/http.png";
+import materialImage from "../assets/image/materialui2.png";
+import testImage from "../assets/image/test.png";
+import storybookImage from "../assets/image/storybook.png";
+import gitImage from "../assets/image/git.png";
+import gitHubImage from "../assets/image/github.png";
+import postmanImage from "../assets/image/postman.png";
 
 export const Skills = () => {
     return (
@@ -11,13 +25,21 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title_h2 text={"Skills"}/>
                 <div className={style.skills}>
-                    <Skill title="JS"
-                           description="I know JS "/>
-                    <Skill title="HTML" description="I know HTML"/>
-                    <Skill title="CSS" description="I know CSS"/>
-                    <Skill title="CSS" description="I know CSS"/>
-                    <Skill title="CSS" description="I know CSS"/>
-                    <Skill title="CSS" description="I know CSS"/>
+                    <Skill title="React" icon={reactImage}/>
+                    <Skill title="TypeScript" icon={tsImage}/>
+                    <Skill title="JavaScript" icon={jsImage}/>
+                    <Skill title="Redux" icon={reduxImage}/>
+                    <Skill title="Rest API" icon={restapiImage}/>
+                    <Skill title="HTTP/HTTPS" icon={httpImage}/>
+                    <Skill title="HTML" icon={htmlImage}/>
+                    <Skill title="CSS" icon={cssImage}/>
+                    <Skill title="SASS" icon={sassImage}/>
+                    <Skill title="Material-UI" icon={materialImage}/>
+                    <Skill title="UNIT TESTS, TDD" icon={testImage}/>
+                    <Skill title="Storybook" icon={storybookImage}/>
+                    <Skill title="GitHub" icon={gitHubImage}/>
+                    <Skill title="GIT" icon={gitImage}/>
+                    <Skill title="Postman" icon={postmanImage}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -5,7 +5,7 @@ import style from './Skill.module.scss';
 export const Skill = (props) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+            <div className={style.icon}><img src={props.icon} alt="icon" className={style.img}/></div>
             <h3>{props.title}</h3>
             <span className={style.description}>{props.description}</span>
         </div>
