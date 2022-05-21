b32d28ea475d51922f057132ba3a90b99748b2bf
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-21 23:35:40 +0300

    add icon in Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import style from './Footer.module.scss';
 import styleContainer from "../common/styles/Container.module.css";
-
+import facebookImage from "../assets/image/facebook.png";
+import linkedinImage from "../assets/image/linkedin.png";
+import telegramImage from "../assets/image/telegram.png";
+import githubImage from "../assets/image/github.png";
 
 export const Footer = () => {
+
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
                 <h2>Volha Mitsiurova</h2>
                 <div className={style.footers}>
-                    <div className={style.footer}></div>
-                    <div className={style.footer}></div>
-                    <div className={style.footer}></div>
-                    <div className={style.footer}></div>
+                    <div className={style.footer}>
+                        <a href='https://www.facebook.com/oliushka.mitsiurova'>
+                        <img src={facebookImage} alt="facebook" className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.footer}>
+                        <a href='https://www.linkedin.com/in/olga-mitsiurova-214181232/'>
+                            <img src={linkedinImage} alt="linkedin" className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.footer}>
+                        <a href='https://t.me/I_am_Olja'>
+                            <img src={telegramImage} alt="telegram" className={style.icon}/>
+                        </a>
+                    </div>
+                    <div className={style.footer}>
+                        <a href='https://github.com/ole4ka140592'>
+                            <img src={githubImage} alt="github" className={style.icon}/>
+                        </a>
+                    </div>
                 </div>
                 <span>©All rights reserved, 2022</span>
             </div>
