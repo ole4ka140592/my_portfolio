0628d324ec1443f7a1af598aa81754ad235048eb
ole4ka140592 <ole4ka140592@gmail.com>
2022-05-29 04:13:53 +0300

    add changes in api, while back is heroku.com

--- src/contacts/api.js
+++ src/contacts/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+
 //api
 export const api = {
     sendMessage({name, email, message}) {
-        return axios.post("http://localhost:3000/sendMessage", {name, email, message})
+        return axios.post("https://portfolio-my-js.herokuapp.com/sendMessage", {name, email, message})
     },
 }
